01c4aa06f1e30cd78d15d33c680beadd09e082a5
Mehak <mehakgarg9111@gmail.com>
2024-09-13 15:17:13 +0530

    first

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Spinner } from "@/components/spinner";
+import Home from "./home";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+
+export default function Page() {
+
+    return (
+        <>
+        
+        <Home/>
+        </>
+    )
+  }
